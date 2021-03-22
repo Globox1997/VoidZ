3c1657ef23751e11d5464a6404f8a57f61065bf5
Globox1997 <mitschke11@web.de>
2021-03-22 12:11:41 +0100

    added infested void block

--- src/main/java/net/voidz/init/BlockInit.java
+++ src/main/java/net/voidz/init/BlockInit.java
@@ -13,22 +13,27 @@ import net.voidz.block.VoidBlock;
 import net.voidz.block.entity.PortalBlockEntity;
 
 public class BlockInit {
+        // Block
+        public static final PortalBlock PORTAL_BLOCK = new PortalBlock();
+        public static final VoidBlock VOID_BLOCK = new VoidBlock(FabricBlockSettings.copy(Blocks.END_STONE));
+        public static final VoidBlock INFESTED_VOID_BLOCK = new VoidBlock(FabricBlockSettings.copy(Blocks.END_STONE));
+        // Entity
+        public static BlockEntityType<PortalBlockEntity> PORTAL_BLOCK_ENTITY;
 
-    public static final PortalBlock PORTAL_BLOCK = new PortalBlock();
-    public static final VoidBlock VOID_BLOCK = new VoidBlock(FabricBlockSettings.copy(Blocks.END_STONE));
-
-    public static BlockEntityType<PortalBlockEntity> PORTAL_BLOCK_ENTITY;
-
-    public static void init() {
-        Registry.register(Registry.ITEM, new Identifier("voidz", "void_portal"),
-                new BlockItem(PORTAL_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-        Registry.register(Registry.BLOCK, new Identifier("voidz", "void_portal"), PORTAL_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("voidz", "void"),
-                new BlockItem(VOID_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-        Registry.register(Registry.BLOCK, new Identifier("voidz", "void"), VOID_BLOCK);
-
-        PORTAL_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, "voidz:void_portal_entity",
-                BlockEntityType.Builder.create(PortalBlockEntity::new, PORTAL_BLOCK).build(null));
-    }
+        public static void init() {
+                // Block
+                Registry.register(Registry.ITEM, new Identifier("voidz", "void_portal"),
+                                new BlockItem(PORTAL_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.BLOCK, new Identifier("voidz", "void_portal"), PORTAL_BLOCK);
+                Registry.register(Registry.ITEM, new Identifier("voidz", "void"),
+                                new BlockItem(VOID_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.BLOCK, new Identifier("voidz", "void"), VOID_BLOCK);
+                Registry.register(Registry.ITEM, new Identifier("voidz", "infested_void"), new BlockItem(
+                                INFESTED_VOID_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.BLOCK, new Identifier("voidz", "infested_void"), INFESTED_VOID_BLOCK);
+                // Entity
+                PORTAL_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, "voidz:void_portal_entity",
+                                BlockEntityType.Builder.create(PortalBlockEntity::new, PORTAL_BLOCK).build(null));
+        }
 
 }
