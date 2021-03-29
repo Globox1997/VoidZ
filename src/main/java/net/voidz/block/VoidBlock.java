b3b9d85a3a106e9bb72fb41a96d23a8329df8280
Globox1997 <mitschke11@web.de>
2021-03-29 21:31:29 +0200

    added activated property

--- src/main/java/net/voidz/block/VoidBlock.java
+++ src/main/java/net/voidz/block/VoidBlock.java
@@ -7,13 +7,23 @@ import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class VoidBlock extends Block {
 
+    public static final BooleanProperty ACTIVATED;
+
     public VoidBlock(Settings settings) {
         super(settings);
+        this.setDefaultState((BlockState) ((BlockState) this.stateManager.getDefaultState()).with(ACTIVATED, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(ACTIVATED);
     }
 
     @Environment(EnvType.CLIENT)
@@ -22,6 +32,10 @@ public class VoidBlock extends Block {
             world.addParticle(ParticleTypes.PORTAL, (double) pos.getX() + random.nextDouble(),
                     (double) pos.getY() + 1.08D, (double) pos.getZ() + random.nextDouble(), 0.0D, 0.0D, 0.0D);
         }
+    }
+
+    static {
+        ACTIVATED = BooleanProperty.of("activated_void");
     }
 
 }
