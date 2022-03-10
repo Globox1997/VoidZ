f95184d5213861fa781449a8a71e9e025b7bd7fc
Globox1997 <mitschke11@web.de>
2022-03-10 11:30:54 +0100

    removed custom removal feature

--- src/main/java/net/voidz/mixin/MinecraftClientMixin.java
+++ src/main/java/net/voidz/mixin/MinecraftClientMixin.java
@@ -4,7 +4,6 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
@@ -30,19 +29,16 @@ public class MinecraftClientMixin {
     @Nullable
     public ClientPlayerInteractionManager interactionManager;
 
-    @Inject(method = "getMusicType", at = @At(value = "HEAD"), cancellable = true)
+    @Inject(method = "getMusicType", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getBlockPos()Lnet/minecraft/util/math/BlockPos;"), cancellable = true)
     public void getMusicTypeMixin(CallbackInfoReturnable<MusicSound> info) {
-        if (this.player != null && this.player.world.getRegistryKey() == DimensionInit.VOID_WORLD) {
+        if (this.player.world.getRegistryKey() == DimensionInit.VOID_WORLD)
             info.setReturnValue(MusicType.DRAGON);
-        }
-
     }
 
     @Inject(method = "doItemUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getCount()I"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
     private void doItemUseMixin(CallbackInfo info, Hand[] var1, int var2, int var3, Hand hand, ItemStack itemStack) {
-        if (player != null && !player.isCreative() && itemStack.getItem() instanceof BlockItem && player.world.getRegistryKey() == DimensionInit.VOID_WORLD) {
+        if (player != null && !player.isCreative() && itemStack.getItem() instanceof BlockItem && player.world.getRegistryKey() == DimensionInit.VOID_WORLD)
             info.cancel();
-        }
     }
 
     @Inject(method = "handleBlockBreaking", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/hit/BlockHitResult;getSide()Lnet/minecraft/util/math/Direction;"), cancellable = true)
@@ -51,11 +47,6 @@ public class MinecraftClientMixin {
             interactionManager.cancelBlockBreaking();
             info.cancel();
         }
-    }
-
-    @ModifyVariable(method = "Lnet/minecraft/client/MinecraftClient;startIntegratedServer(Ljava/lang/String;Lnet/minecraft/util/registry/DynamicRegistryManager$Impl;Ljava/util/function/Function;Lcom/mojang/datafixers/util/Function4;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient$WorldLoadAction;NONE:Lnet/minecraft/client/MinecraftClient$WorldLoadAction;", ordinal = 0), ordinal = 2, require = 0)
-    private boolean startIntegratedServerMixin(boolean original) {
-        return false;
     }
 
 }
